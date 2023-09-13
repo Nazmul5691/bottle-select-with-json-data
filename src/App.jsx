f1cdf3cc8e02ad27f6dc3e8bb520c4e36bf259c1
Nazmul5691 <nazmulislam5691@gmail.com>
2023-09-14 00:01:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import { useEffect } from 'react'
+import Bottles from './components/Bottles/Bottles'
+
+function App() {
+
+
+  return (
+    <>
+        <Header></Header>
+        <Bottles></Bottles>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Memorable Water Bottle</h1>
+            
+        </div>
+    );
+};
+
+export default Header;
